ecc28a2a5c85e5ff378197477a9b83268cf97d0b
Yoann Chane Kive <yoannck@gmail.com>
2016-12-23 18:57:16 +1300

    scrape with authentification

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var browser = require('zombie');
 
 /**
  * Web Scraper
+ * Author : Yoann Chane Kive
  */
 module.exports = {
+  extract: function(html, filter) {
+    var datas = [];
+    var $ = cheerio.load(html);
+    $(filter).each(function() {
+      var data = $(this);
+      datas.push({text: data.text().replace(/\r?\n|\r/g,'').trim()});
+    })
+    return datas;
+  }
   /**
    * Get datas from a URL with a parameter filter
    *
@@ -16,16 +27,32 @@ module.exports = {
      return new Promise(function (success, reject){
        request(url, function(error, response, html) {
          if (!error) {
-           var datas = [];
-           var $ = cheerio.load(html);
-           $(filter).each(function() {
-             var data = $(this);
-             datas.push({text: data.text().replace(/\r?\n|\r/g,'').trim()});
-           })
-           success(datas);
+           success(extract(html, filter));
          } else {
            reject(err);
          }
+       });
+     });
+   };
+   /**
+    * Get datas from a URL (Authentification Required) with a parameter filter
+    *
+    * @param  {Object} parameters
+    * @return {Promise}
+    */
+   getWithAuthentification(parameters) {
+     return new Promise(function (success, reject){
+       var browser = new Browser();
+       browser.visit(parameters.login.url, function(){
+         browser.fill(parameters.login.selectorUser, parameters.login.user)
+           .fill(parameters.login.selectorPassword, parameters.login.password)
+           .pressButton(parameters.login.selectorSubmit, function() {
+               var datas = [];
+               browser.visit(parameters.wanted.url, function(){
+                 success(extract(browser.html(), parameters.wanted.filter));
+                // reject(err);
+               });
+           });
        });
      });
    }
